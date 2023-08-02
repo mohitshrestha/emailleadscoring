6af669a592904065033a634a505e4c9e462324ee
Mohit Shrestha <mohitshrestha@gmail.com>
2023-08-01 21:09:56 -0700

    Add `cost_calculations.py` that contains all the cost calculation factions; update index

--- README.md
+++ README.md
@@ -16,7 +16,44 @@ pip install emailleadscoring
 Fill me in please! Don’t forget code examples:
 
 ``` python
-1+1
+import emailleadscoring as els
 ```
 
-    2
+Testing: Cost Table Function
+
+``` python
+# els.cost_calc_monthly_cost_table()
+```
+
+Testing: Total Cost Function
+
+``` python
+# els.cost_total_unsub_cost(els.cost_calc_monthly_cost_table())
+```
+
+``` python
+import pandas_flavor as pf
+# - Using pandas_flavor package and adding @pf.register_dataframe_method # Using pandas_flavor package: @pf.register_dataframe_method** Turns a function into a data frame method so we can use method chaining
+
+# import email_lead_scoring as els
+
+# els.cost_calc_monthly_cost_table() \
+#     .cost_total_unsub_cost()
+```
+
+Testing: Simulation Function
+
+``` python
+# els.cost_simulate_unsub_costs()
+```
+
+Testing: Plot Simulation (Heat Map) Function
+
+``` python
+# import email_lead_scoring as els
+
+# els.cost_simulate_unsub_costs(
+#     email_list_monthly_growth_rate = [0, 0.015, 0.025, 0.035]
+# ) \
+#     .cost_plot_simulated_unsub_costs()
+```
--- emailleadscoring/_modidx.py
+++ emailleadscoring/_modidx.py
@@ -5,4 +5,11 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://mohitshrestha.github.io',
                 'git_url': 'https://github.com/mohitshrestha/emailleadscoring',
                 'lib_path': 'emailleadscoring'},
-  'syms': {'emailleadscoring.core': {'emailleadscoring.core.foo': ('core.html#foo', 'emailleadscoring/core.py')}}}
+  'syms': { 'emailleadscoring.cost_calculations': { 'emailleadscoring.cost_calculations.cost_calc_monthly_cost_table': ( 'cost_calculations.html#cost_calc_monthly_cost_table',
+                                                                                                                         'emailleadscoring/cost_calculations.py'),
+                                                    'emailleadscoring.cost_calculations.cost_plot_simulated_unsub_costs': ( 'cost_calculations.html#cost_plot_simulated_unsub_costs',
+                                                                                                                            'emailleadscoring/cost_calculations.py'),
+                                                    'emailleadscoring.cost_calculations.cost_simulate_unsub_costs': ( 'cost_calculations.html#cost_simulate_unsub_costs',
+                                                                                                                      'emailleadscoring/cost_calculations.py'),
+                                                    'emailleadscoring.cost_calculations.cost_total_unsub_cost': ( 'cost_calculations.html#cost_total_unsub_cost',
+                                                                                                                  'emailleadscoring/cost_calculations.py')}}}
